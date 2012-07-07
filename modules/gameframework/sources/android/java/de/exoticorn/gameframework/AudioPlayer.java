2cde987ce0a7f3dc652bf9cdcaa2a528cdc0b59b
Dennis Ranke <mail@dennisranke.de>
2012-07-07 19:01:00 +0200

    fix cpu spin when sound is stopped on android

--- modules/gameframework/sources/android/java/de/exoticorn/gameframework/AudioPlayer.java
+++ modules/gameframework/sources/android/java/de/exoticorn/gameframework/AudioPlayer.java
@@ -3,11 +3,16 @@ package de.exoticorn.gameframework;
 import android.media.AudioTrack;
 import android.media.AudioFormat;
 import android.media.AudioManager;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
 
 public class AudioPlayer extends Thread {
 	long m_gameFramework;
 	AudioTrack m_track;
 	short[] m_buffer;
+	final ReentrantLock m_lock = new ReentrantLock();
+	final Condition m_playingCondition = m_lock.newCondition();
+	boolean m_isPlaying = true;
 	
 	public AudioPlayer(long gameFramework)
 	{
@@ -22,6 +27,22 @@ public class AudioPlayer extends Thread {
 		m_track.play();
 		while(true)
 		{
+			m_lock.lock();
+			try
+			{
+				while(!m_isPlaying)
+				{
+					m_playingCondition.await();
+				}
+			}
+			catch(InterruptedException e)
+			{
+				break;
+			}
+			finally
+			{
+				m_lock.unlock();
+			}
 			Native.fillAudioBuffer(m_gameFramework, m_buffer);
 			m_track.write(m_buffer, 0, m_buffer.length);
 		}
@@ -30,10 +51,17 @@ public class AudioPlayer extends Thread {
 	public void onPause()
 	{
 		m_track.stop();
+		m_lock.lock();
+		m_isPlaying = false;
+		m_lock.unlock();
 	}
 	
 	public void onResume()
 	{
 		m_track.play();
+		m_lock.lock();
+		m_isPlaying = true;
+		m_playingCondition.signal();
+		m_lock.unlock();
 	}
 }
